598199daf307fdcd778f9b734eb5de4ab4ae2c87
Anton Astashov <anton.astashov@gmail.com>
2016-05-30 14:07:30 -0500

    Fix typo in monit_generator.sh

--- monit_generator.sh
+++ monit_generator.sh
@@ -7,7 +7,7 @@ case $1 in
    start)
       echo $$ > /var/run/crossdart_server_generator.pid;
       mkdir -p $DIRROOT/logs
-      exec 2>&1 dart $DIRROOT/bin/server.dart --dirroot $DIRROOT 1>>$DIRROOT/logs/log_generator.txt
+      exec 2>&1 dart $DIRROOT/bin/generator.dart --dirroot $DIRROOT 1>>$DIRROOT/logs/log_generator.txt
       ;;
     stop)
       kill `cat /var/run/crossdart_server_generator.pid` ;;
